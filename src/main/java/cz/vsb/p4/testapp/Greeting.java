696463943656e0f2c12faf66be59b06b38955fc2
ruz76 <jan.ruzicka.vsb@gmail.com>
2016-10-06 10:14:17 +0200

    Simple REST

---
+++ src/main/java/cz/vsb/p4/testapp/Greeting.java
@@ -0,0 +1,22 @@
+package cz.vsb.p4.testapp;
+
+/**
+ * Created by ruz76 on 6.10.2016.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
